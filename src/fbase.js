53c4590ecc25a4a0e5559b3ab0c0e215b7bc2b4e
chs52699 <chs52699@naver.com>
2022-05-18 17:53:07 +0900

    Edit/Cancle button add

--- src/fbase.js
+++ src/fbase.js
@@ -3,7 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 // push testing
 const firebaseConfig = {
   apiKey: "AIzaSyDFJzAEwTuo4my8nQ4fsIS_ktyXv2YhN4g",
@@ -12,7 +11,7 @@ const firebaseConfig = {
   storageBucket: "gongsa-d233a.appspot.com",
   messagingSenderId: "772155270923",
   appId: "1:772155270923:web:d64c6b9118ca3cfee8640e",
-  measurementId: "G-7F6MDW2NBE"
+  measurementId: "G-7F6MDW2NBE",
 };
 
 const app = initializeApp(firebaseConfig);
