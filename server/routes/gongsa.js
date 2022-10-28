04062290f52c97516076db4d8344f49afff539f5
2-jun9 <jungu269@gmail.com>
2022-10-28 17:27:54 +0900

    Add: mongoDB 쿼리문 작성

--- server/routes/gongsa.js
+++ server/routes/gongsa.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 router.route("/").get(async (req, res, next) => {
   try {
-    res.send("OK.");
-    const gongsa = await Gongsa.find({});
-    console.log(gongsa);
+    const gongsa = await Gongsa.find().sort({ createdAt: -1 }).limit(5);
+    // 날짜 순 정렬 및 5개 제한하는 쿼리, 해당 쿼리에서 sorting하는 부분만 파라미터로 받아와서 반영할 수 있게 하면 될듯
+    res.send(gongsa);
   } catch (err) {
     console.error(err);
     next(err);
